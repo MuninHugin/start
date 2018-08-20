f0b200f7b8ef513e3814da00b79503f31f2155cf
MuninHugin <workbox33@yandex.ru>
2018-08-20 23:24:14 +0300

    Lesson 6

--- classes/Car.php
+++ classes/Car.php
@@ -5,6 +5,10 @@ class Car
 	public $color;
 	public $brand;
 	public $doors;
+	public static $count = 0;
+
+	const CAR_PROTOTYPE = "Прототип";
+	const CAR_PROTOTYPE_SPEED = 150;
 
 	public function __construct($color, $brand, $doors = 5, $year) {
 		$this->color = $color;
@@ -20,5 +24,14 @@ class Car
 		Кол-во дверей: {$this->doors}<br>
 		Год выпуска: {$this->year}<br>
 		===========================<br>";
+	}
+
+	public static function getCountCar(){
+		echo self::$count++ . "<br>";
+	}
+	public static function getPrototypeCar(){
+		echo "<h3>Данные тестового авто:</h3>
+		<b>Имя:</b> ". self::CAR_PROTOTYPE ."<br>
+		<b>Скорость:</b> ". self::CAR_PROTOTYPE_SPEED ."<br>";
 	}
 }
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require_once 'classes/Car.php';
 
+echo Car::$count."<br>";
+Car::getCountCar();
 $car1 = new Car("Зелёный", "Лада калина", 5, 2017);
+Car::getCountCar();
 $car2 = new Car("Серебристый", "Нива", 3, 2014);
+Car::getCountCar();
 
 echo $car1->getCarInfo();
 echo $car2->getCarInfo();
+Car::getPrototypeCar();
+echo Car::CAR_PROTOTYPE."<br>";
