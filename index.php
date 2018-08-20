b0f789731b735fae008a701af7c206a7a18b979f
MuninHugin <workbox33@yandex.ru>
2018-08-20 22:32:06 +0300

    Write to file

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-require_once 'classes/Car.php';
+require_once 'classes/File.php';
 
-$car1 = new Car("Зелёный", "Лада калина", 5, 2017);
-$car2 = new Car("Серебристый", "Нива", 3, 2014);
-
-echo $car1->getCarInfo();
-echo $car2->getCarInfo();
+$file = new File(__DIR__.'/file.txt');
+$file->write("Test writing.");
